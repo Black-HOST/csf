7d1558d7491ed575d1a9a958047de8ed3a147745
Zeroth <ectronx@gmail.com>
2026-03-11 14:11:32 +0100

    refactor(sanity): move sanity.txt into conf

--- install.cwp.sh
+++ install.cwp.sh
@@ -382,7 +382,7 @@ cp -avf uninstall.cwp.sh /usr/local/csf/bin/uninstall.sh
 cp -avf csftest.pl /usr/local/csf/bin/
 cp -avf remove_apf_bfd.sh /usr/local/csf/bin/
 cp -avf readme.txt /etc/csf/
-cp -avf sanity.txt /usr/local/csf/lib/
+cp -avf conf/sanity.txt /usr/local/csf/lib/sanity.txt
 cp -avf csf.rbls /usr/local/csf/lib/
 cp -avf restricted.txt /usr/local/csf/lib/
 cp -avf changelog.txt /etc/csf/
